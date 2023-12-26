d1c2efc280c2227acd1b9d0c5054b9c71b704894
michaelkp123 <72190672+michaelkp123@users.noreply.github.com>
2023-12-26 22:59:45 +0100

    Add hardcoded task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../../services/task.service' 
+import { Task } from '../../Task';
+import { TaskItemComponent } from '../task-item/task-item.component';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [CommonModule, TaskItemComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+  
+  ngOnInit(): void {
+   this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
+  }
+
+}
